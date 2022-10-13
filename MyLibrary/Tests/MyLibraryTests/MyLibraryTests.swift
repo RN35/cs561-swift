2dc3ac366ada08aa9e5d4dd52681f59afda237d7
Rohit <rohitnair35@gmail.com>
2022-10-13 13:41:06 -0700

    Added intergrationn tests and tests for weather data model

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,7 +1,8 @@
 import XCTest
-import MyLibrary
+@testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
+    //Unit Tests
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
         // Given
         let mockWeatherService = MockWeatherService(
@@ -67,6 +68,55 @@ final class MyLibraryTests: XCTestCase {
 
         // Then
         XCTAssertNil(isLuckyNumber)
+    }
+    
+    
+    // Integration Tests
+    func testIsLuckyBecauseWeAlreadyHaveLuckyNumberActual() async {
+        // Given
+
+        let myLibrary = MyLibrary()
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(8)
+
+        // Then
+        XCTAssertNotNil(isLuckyNumber)
+        XCTAssert(isLuckyNumber == true)
+    }
+    
+    func testIsLuckyBecauseWeatherHasAnEightActual() async throws {
+        // Given
+
+        let myLibrary = MyLibrary()
+
+        // When
+        let isLuckyNumber = await myLibrary.isLucky(0)
+
+        // Then
+        XCTAssertNotNil(isLuckyNumber)
+        XCTAssert(isLuckyNumber == true)
+    }
+    func testWeatherModel_ShouldPassIfTempIsValid() {
+        let decoder = JSONDecoder()
+        let data = Data("{\"main\": {\"temp\":58}}".utf8)
+        do {
+            let container = try decoder.decode(Weather.self, from: data)
+            XCTAssertNotNil(container)
+            XCTAssert(container.main.temp == 58.0)
+        } catch {
+            XCTFail("Failed to decode temp from json")
+        }
+    }
+    func testWeatherModel_ShouldFailIfTempIsInvalid() {
+        let decoder = JSONDecoder()
+        let data = Data("{\"main\": {\"temp\":\"invalid temp\"}}".utf8)
+        do {
+            let container = try decoder.decode(Weather.self, from: data)
+            XCTFail("Should not have been possible to decode temp for invalid input")
+        } catch {
+            XCTAssert(true == true)
+        }
     }
 
 }
